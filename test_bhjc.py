6b749278451a20f107371cdc7b904b51d19e2b60
Keith Landry <keith.landry@geniussports.com>
2018-04-03 09:54:16 -0700

    0 thresh for ssd for mAP

--- test_bhjc.py
+++ test_bhjc.py
@@ -27,7 +27,7 @@ parser.add_argument('--trained_model', default='weights/ssd1166_300_iter76000.pt
                     type=str, help='Trained state_dict file path to open')
 parser.add_argument('--save_folder', default='eval/', type=str,
                     help='Dir to save results')
-parser.add_argument('--visual_threshold', default=0.2, type=float,
+parser.add_argument('--visual_threshold', default=0, type=float,
                     help='Final confidence threshold')
 parser.add_argument('--cuda', default=False, type=bool,
                     help='Use cuda to train model')
@@ -37,6 +37,7 @@ parser.add_argument('--ball_only', default=True, type=str2bool)
 parser.add_argument('--square_boxes', default=True, type=str2bool)
 parser.add_argument('--anno_dir', default='/home/ec2-user/computer_vision/bball_detection/ssd.pytorch/data/bhjc20180123_bball/annotations/')
 parser.add_argument('--img_dir', default='/home/ec2-user/computer_vision/bball_detection/ssd.pytorch/data/bhjc20180123_bball/images/')
+parser.add_argument('--outname', default='bbox_predictions_ssd300_testonly_thresh.0.json')
 
 args = parser.parse_args()
 
@@ -61,7 +62,9 @@ def test_net(save_folder, net, cuda, testset, transform, net_name):
         # img = testset.pull_image(i)
         # img_id, img, annotation = testset.pull_image_anno(i)
         img_id, img = testset.pull_image(i)
-        # img_id, annotation = testset.pull_anno(i)
+
+        # this .copy line makes a huge difference (at least on lookahead architecture)
+        x = x[:, :, ::-1].copy()
         x = torch.from_numpy(transform(img)[0]).permute(2, 0, 1)
         x = Variable(x.unsqueeze(0))
 
@@ -135,7 +138,8 @@ if __name__ == '__main__':
         test_image_ids = f.readlines()
         test_image_ids = [im_id.rstrip() for im_id in test_image_ids]
 
-    test_image_ids = [str(i).zfill(5) for i in range(800, 1805)]
+    # use unannotated images instead
+    # test_image_ids = [str(i).zfill(5) for i in range(800, 1805)]
 
     # test_image_ids = ['00700', '00701']
 
