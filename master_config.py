92fafd42aac0b980f7211077436fa6cb820a2b87
Keith Landry <keith.landry@geniussports.com>
2018-04-04 15:31:44 -0700

    adding config for small583

--- master_config.py
+++ master_config.py
@@ -64,5 +64,27 @@ configs = {
             'name': 'v2',
             'center_step_size': 1
         }
+    },
+
+    'small': {
+        'base': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'C', 512, 512, 512, 'M', 512, 512, 512],
+        'layers5to7': True,
+        'extras': [256, 'S', 512, 128, 'S', 256, 128, 256, 128, 256],
+        'mbox': [4, 6, 6, 6, 4, 4],
+        'vgg_source': [21, -2],
+        'final_base_layer_dim': 1024,
+        'box_configs': {
+            'feature_maps': [73, 37, 19, 10, 8, 6],
+            'min_dim': 1166,
+            'steps': [8, 16, 32, 64, 69, 78],  # default for 1166
+            'min_sizes': [30, 60, 111, 162, 213, 264],  # default for 300
+            'max_sizes': [60, 111, 162, 213, 264, 315],  # default for 300
+            'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]],  # default for 300
+            'square_only': False,
+            'variance': [0.1, 0.2],
+            'clip': True,
+            'name': 'v2',
+            'center_step_size': 1
+        }
     }
 }
