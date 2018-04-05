b582c87a3aff54ae8035a9bbf394ef33f1d6929b
Keith Landry <keith.landry@geniussports.com>
2018-04-05 13:06:10 -0700

    print img size

--- data/bhjc20180123_bball/bhjc.py
+++ data/bhjc20180123_bball/bhjc.py
@@ -179,11 +179,11 @@ class BhjcBballDataset(data.Dataset):
         if self.transform is not None:
             target = np.array(target)
             img, boxes, labels = self.transform(img, target[:, :4], target[:, 4])
+            print(img.size)
             # to rgb
             img = img[:, :, (2, 1, 0)]
             # img = img.transpose(2, 0, 1)
             target = np.hstack((boxes, np.expand_dims(labels, axis=1)))
-            print(img.size)
         return torch.from_numpy(img).permute(2, 0, 1), target, height, width
         # return torch.from_numpy(img), target, height, width
 
